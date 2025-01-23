3e11151afdf117cfc6cd0c59e7831197bbb367d2
pedrohenrique0121 <pedro.dourado@neomed.tech>
2025-01-23 16:44:51 -0300

    Feat: add properties, Game ORM

---
+++ src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -0,0 +1,26 @@
+package com.devsuperior.dslist.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="tb_game")
+public class Game {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    @Column(name="game_year")
+    private String year;
+    private String genre;
+    private String platforms;
+    private Double score;
+    private String imageUrl;
+    private String shortDescription;
+    private String longDescription;
+
+}
